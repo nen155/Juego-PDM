461d5948c0fdcd6d14d3734525dffc4b708077ce
nen155 <nen156@gmail.com>
2017-03-10 12:03:02 +0100

    Nuevo Proyecto
    Android nativo

---
+++ LearningMixColor/LearningMixColor/app/src/main/java/com/redforest/com/learningmixcolor/CanvasView.java
@@ -0,0 +1,97 @@
+package com.redforest.com.learningmixcolor;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by NeN on 09/03/2017.
+ */
+
+public class CanvasView extends View {
+    private int height,weight;
+    private Bitmap bitmap;
+    private Canvas canvas;
+    private Path path;
+    private Paint paint;
+    private float mX,mY;
+    private static final float TOLERANCE = 5;
+    Context context;
+
+    public CanvasView(Context context, AttributeSet attributeSet) {
+        super(context,attributeSet);
+        this.context = context;
+
+        path = new Path();
+
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeWidth(4f);
+
+    }
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        canvas.drawPath(path,paint);
+    }
+    @Override
+    protected void onSizeChanged(int w,int h, int oldw, int oldh){
+        super.onSizeChanged(w,h,oldw,oldh);
+        bitmap = Bitmap.createBitmap(w,h,Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+
+    }
+    private void startTouch(float x,float y){
+        path.moveTo(x,y);
+        mX=x;
+        mY=y;
+    }
+
+    private void moveTouch(float x, float y){
+        float dx = Math.abs(x-mX);
+        float dy =Math.abs(y-mY);
+        if(dx >=TOLERANCE || dy >= TOLERANCE){
+            path.quadTo(mX,mY,(x+mX)/2,(y+mY)/2);
+            mX=x;
+            mY=y;
+        }
+    }
+    public void clearCanvas(){
+        path.reset();
+        invalidate();
+    }
+    private void upTouch(){
+        path.lineTo(mX,mY);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent){
+        float x = motionEvent.getX();
+        float y = motionEvent.getY();
+        switch (motionEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                startTouch(x,y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveTouch(x,y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                upTouch();
+                invalidate();
+                break;
+
+
+        }
+        return true;
+    }
+}
---
+++ LearningMixColor/LearningMixColor/app/src/main/java/com/redforest/com/learningmixcolor/MainActivity.java
@@ -0,0 +1,25 @@
+package com.redforest.com.learningmixcolor;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private CanvasView canvasView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        canvasView = (CanvasView) findViewById(R.id.canvasView);
+
+    }
+    public void clearCanvas(View v){
+
+    }
+}
